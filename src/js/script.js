b1373796869f6119c4d4b502368845551ce33c8d
Ilya <lilapank@mail.ru>
2023-05-16 07:27:24 +0300

    add_carusel

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="icons/leftarrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="icons/rightarrow.png"> </button>'
+        });
+});
