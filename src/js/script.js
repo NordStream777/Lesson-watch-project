2cf02ce53fd8c4a9c64c71430270e0454fd2f984
Ilya <lilapank@mail.ru>
2023-06-08 14:07:44 +0300

    JS

--- src/js/script.js
+++ src/js/script.js
@@ -25,4 +25,43 @@ $(document).ready(function(){
         }
         toggleSlide('.catalog-item__link')
         toggleSlide('.catalog-item__back')
+
+        $('[data-modal=consultation]').on('click', function(){
+            $('.overlay, #consultation').fadeIn('slow');
+        });
+
+        $('.modal__close').on('click', function(){
+            $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        });
+
+        $('.button__item').each (function(i){
+            $(this).on ('click', function(){
+                $('#order .modal__descr').text($('catalog-item__subtitle').eq(i).text());
+                $('.overlay, #order').fadeIn('slow');
+            });
+        });
+
+        function validateForms(form){
+            $(form).validate({
+                rules:{
+                    name: "required",
+                    tel: "required",
+                    email:{
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: "Укажи свое имя",
+                    tel: "Введи свой номер",
+                    email:{
+                        required: "Хотим тебе ништяки на почту присылать",
+                        email: "Не наебешь"
+                    }
+                }
+            });
+        };
+        validateForms('#consultation-form');
+        validateForms('#consultation form');
+        validateForms('#order form');
 });
